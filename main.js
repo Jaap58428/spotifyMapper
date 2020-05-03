0411af5af4e3939ac35a9830bcd30f7f8948cf8a
Ju1111 <schneiderju@gmx.net>
2020-05-03 12:26:48 +0200

    added base class for cities with test code in main function

--- main.js
+++ main.js
@@ -1,5 +1,28 @@
 const main = () => {
+    var london = new City("London");
+    london.addToList("Nothing But Thieves");
+    london.addToList("Frank Carter");
+    console.log(london.bandCount);
     console.log("Scripts work!");
+}
+
+class City {
+  bandArray = [];
+
+  constructor(cityName)
+  {
+    this.name = cityName;
+  }
+
+  addToList(band)
+  {
+    this.bandArray.push(band);
+  }
+
+  get bandCount()
+  {
+    return this.bandArray.length;
+  }
 }
 
 window.onload = main
