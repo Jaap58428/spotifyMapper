e26762d8f8f97dc21551c30cc595afc0fee18cbd
Jaap Kanbier <jaapkanbier@hotmail.com>
2020-05-03 12:20:47 +0200

    working auth

--- main.js
+++ main.js
@@ -2,32 +2,42 @@
 const client_id = 'ba8b2e03236a45b0828c5e3573b316fa'
 
 const authenticateUser = () => {
-    const auth_url = "https://accounts.spotify.com/authorize"
-    const repsonse_type = "token"
-    const redirect_uri = "https://itsjaap.nl/projecten/spotifyMapper/"
+    const redirect_uri = encodeURI("https://itsjaap.nl/projecten/spotifyMapper/")
+    window.location.replace(`https://accounts.spotify.com/authorize?client_id=${client_id}&redirect_uri=${redirect_uri}&response_type=token`);
+}
 
-    var xhttp = new XMLHttpRequest();
-    xhttp.onreadystatechange = () => {
-        if (this.readyState == 4 && this.status == 200) {
-            // Typical action to be performed when the document is ready:
-            console.log(xhttp.response);
-        }
+function getUrlVars() {
+    // no clue, just grabbed this from online
+    // https://html-online.com/articles/get-url-parameters-javascript/
+
+    var vars = {};
+    var parts = window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
+        vars[key] = value;
+    });
+    return vars;
+}
+
+const getTokenFromUrl = () => {
+    token = null
+    parameter = 'access_token'
+    if(window.location.href.indexOf(parameter) > -1){
+        token = urlparameter = getUrlVars()[parameter];
+    } 
+
+    return token
+}
+
+const checkToken = () => {
+    token = getTokenFromUrl()
+    if (token) {
+        console.log('Token found:', token);
+    } else if (confirm("There seems to be no token, please login")) {
+        authenticateUser()
     }
-
-    full_url = auth_url + '?client_id=' + client_id + '&redirect_uri=' + redirect_uri
-
-    xhttp.open("GET", full_url , true);
-    xhttp.setRequestHeader('Access-Control-Allow-Origin', '*')
-    xhttp.send();
-    return null
 }
 
 const main = () => {
-    // connect authentication function to button
-    document.getElementById('authButton').addEventListener('click', authenticateUser)
-    
-    console.log("Scripts work!");
-
+    checkToken()
 }
 
 window.onload = main
